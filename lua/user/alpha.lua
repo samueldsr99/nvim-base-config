381b4ad5c00fb149c6d384540daa40b42387d116
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2022-08-29 10:28:04 -0400

    feat: Change alpha image

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -1,39 +1,57 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+  return
 end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-  [[                                                                                                  ]],
-  [[                                                                                                  ]],
-  [[                                                                                                  ]],
-  [[██╗░░░░░░█████╗░███╗░░██╗░██████╗░  ██╗░░░░░██╗██╗░░░██╗███████╗  ███╗░░██╗██╗░░░██╗██╗███╗░░░███╗]],
-  [[██║░░░░░██╔══██╗████╗░██║██╔════╝░  ██║░░░░░██║██║░░░██║██╔════╝  ████╗░██║██║░░░██║██║████╗░████║]],
-  [[██║░░░░░██║░░██║██╔██╗██║██║░░██╗░  ██║░░░░░██║╚██╗░██╔╝█████╗░░  ██╔██╗██║╚██╗░██╔╝██║██╔████╔██║]],
-  [[██║░░░░░██║░░██║██║╚████║██║░░╚██╗  ██║░░░░░██║░╚████╔╝░██╔══╝░░  ██║╚████║░╚████╔╝░██║██║╚██╔╝██║]],
-  [[███████╗╚█████╔╝██║░╚███║╚██████╔╝  ███████╗██║░░╚██╔╝░░███████╗  ██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║]],
-  [[╚══════╝░╚════╝░╚═╝░░╚══╝░╚═════╝░  ╚══════╝╚═╝░░░╚═╝░░░╚══════╝  ╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝]],
-  [[                                                                                                  ]],
-  [[                                      by @samueldsr99                                             ]],
+  [[                        ,                                    ]],
+  [[   ,::.::::.            / `.                                 ]],
+  [[ ;::        `.         (  ,--.                               ]],
+  [[ ,;           :   _,--'"\/""-.\_                             ]],
+  [[,:             `./ ,---./( O )) `;                           ]],
+  [[;  `.           _,'    (  `-' ) /_.                          ]],
+  [[;   :         ,'        \    , (o\\                          ]],
+  [[ ;  :         \  \-.  -.__--'   \' )                         ]],
+  [[ ;  ;         /\ (    `-._`-._   \/                          ]],
+  [[  ';,        ; : |      -.`._'\   `.                         ]],
+  [[    ;       ;  : `-.,-..  )  \'\   ^.                        ]],
+  [[     ;     ;   `.__   )))\ ) (`.\    \                       ]],
+  [[      ;   ;        `-`///(, \ \ \)  ,ooo.                    ]],
+  [[       ;,;      ;     ``  ))))_;'(  88888p                   ]],
+  [[        ;      ;         ((-='--',-,Y8888'                   ]],
+  [[        ;     :         ;     ,:'-'  `"'                     ]],
+  [[         ;     `        ;      |                             ]],
+  [[          ;      (_   __   _,-'                              ]],
+  [[           `---.   ;"(,-' /                              ____]],
+  [[    -hrr-       \ (__/\\_`-.-.                     _____/    ]],
+  [[              ,(( '/\/\/\`-;;))             ______//         ]],
+  [[             ((\''/\/\/\/\/\/`/\      _____/  ____/          ]],
+  [[             /'/\/\/\/\/\/\/\/\/)  __/  _____/               ]],
+  [[            (\/\/\/\/\/\/\/\/\_/ _/ ___/                     ]],
+  [[             `-|""--"-.___,--'|-'__/                         ]],
+  [[               |              | /                            ]],
+  [[               |         __,--'                              ]],
+  [[               _\,----""'                                    ]],
 }
+
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+  dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
--- NOTE: requires the fortune-mod package to work
-	-- local handle = io.popen("fortune")
-	-- local fortune = handle:read("*a")
-	-- handle:close()
-	-- return fortune
-	return "---"
+  -- NOTE: requires the fortune-mod package to work
+  -- local handle = io.popen("fortune")
+  -- local fortune = handle:read("*a")
+  -- handle:close()
+  -- return fortune
+  return "---"
 end
 
 dashboard.section.footer.val = footer()
